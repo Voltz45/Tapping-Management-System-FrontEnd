bd132a375330c3be17d42cc86a3f5eef2d814253
Chiks <chikko45putra@gmail.com>
2021-11-28 20:54:51 +0700

    Fixing Design and function

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {DefaultComponent} from "./layout/default/default.component";
+import {DashboardComponent} from "./modules/dashboard/dashboard.component";
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'TMS-Home/dashboard',
+  component: DefaultComponent,
+  children: [
+    {
+      path: '',
+      component: DashboardComponent
+    }
+  ]
+},
+  {
+    path: '',
+    redirectTo: 'TMS-Home/dashboard',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {DefaultModule} from "./layout/default/default.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        DefaultModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
